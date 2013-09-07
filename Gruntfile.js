5e3dad27d250cc9a47fa5689beb8d447caeed75d
Amos Chan <amosschan@gmail.com>
2013-09-07 01:12:11 -0400

    adds travis-ci support

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,4 +30,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['jshint', 'jasmine']);
+  grunt.registerTask('test', ['jshint', 'jasmine']);
 };
